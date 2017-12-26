793171a6cc4cfea5b9f57b3fe7bd34690a43ed6a
drkchiloll <samwomack_@me.com>
2017-12-26 08:40:57 -0600

    Create properties.ts

---
+++ src/properties.ts
@@ -0,0 +1,12 @@
+export namespace Properties {
+  export const AzureApp = {
+    "clientId": "App ID / Client ID",
+    "authority": "https://login.microsoftonline.com/<<TENANT_ID>>",
+    "scopes": ["user.read", "tasks.readwrite"],
+    "webApi": "https://graph.microsoft.com/",
+    "tenant": "<<YOUR TENANT ID>>",
+    "redirectUri": "<<YOUR URL>>",
+    "contentUrl": "<<MAIN PAGE AS IT WILL BE VIEWED IN THE TAB>>",
+    "websiteUrl": "<<YOURS>>"
+  }
+}
