28abddc02fd696737e9bd2cd52fb578f107d8488
drkchiloll <samwomack_@me.com>
2017-12-27 04:59:39 -0600

    added function to retrieve and parse Outlook Events

--- server/services/outlook-calendars.ts
+++ server/services/outlook-calendars.ts
@@ -1,7 +1,62 @@
 import { MsGraph } from '../lib';
+import * as Promise from 'bluebird';
+import * as moment from 'moment';
+import * as momentTz from 'moment-timezone';
+
+import { timeProc } from '../services/event-dates';
 
 export function outlookServFactory(graph: MsGraph) {
   const service: any = {};
+
+  service.get = function(tz) {
+    let events: any, eventProp: string;
+    return graph._request({
+      body: {},
+      method: 'get',
+      path: '/beta/me/events'
+    }).then(({value}: any) => {
+      events = timeProc.uiDates();
+      if(value && value.length > 0) {
+        return Promise.each(value, (val:any) => {
+          let event: any = {};
+          event.id = val.id;
+          event.subject = val.subject;
+          event.link = val.webLink;
+          event.html = val.body.contentType === 'html' ?
+            val.body.content : '';
+          event.startDate = timeProc.normalizeMsDate({
+            date: val.start.dateTime, tz
+          });
+          event.endDate = timeProc.normalizeMsDate({
+            date: val.end.dateTime, tz
+          });
+          event.location = val.location.displayName || '';
+          event.organizer = {
+            name: val.organizer.emailAddress.name,
+            email: val.organizer.emailAddress.address
+          };
+          eventProp = timeProc.compareDates({
+            date: val.start.dateTime, tz
+          });
+          console.log(eventProp);
+          return Promise.map(val.attendees, (att: any) => ({
+            name: att.emailAddress.name || '',
+            emailAddress: att.emailAddress.address,
+            status: att.status.response,
+            type: att.type
+          })).then(attendees => {
+            console.log(eventProp);
+            event.attendees = attendees;
+            events[eventProp].push(event);
+            return;
+          });
+        }).then(() => {
+          // console.log(events);
+          return events;
+        })
+      }
+    });
+  };
 
   service.createEvent = function(body: any) {
     /*
