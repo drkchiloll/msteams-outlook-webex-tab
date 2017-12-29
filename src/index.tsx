bd47352decda7b2c711ab7fa3cab82bf7c3ea0db
drkchiloll <samwomack_@me.com>
2017-12-28 20:03:20 -0600

    Material-UI setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,39 @@ import * as ReactDOM from 'react-dom';
 import { Router, Route, Switch } from 'react-router';
 import { createBrowserHistory } from 'history';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {
+  purple500, deepPurple500, deepPurple200
+} from 'material-ui/styles/colors';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: deepPurple200,
+    primary1Color: deepPurple500
+  },
+  appBar: {
+    height: 50,
+  }
+});
+
 import { App, AuthDialog, ConfigDialog } from './containers';
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history}>
-    <Switch>
-      <Route exact path="/" component={ App } />
-      <Route path="/config" component={ ConfigDialog } />
-      <Route path="/auth" component={ AuthDialog } />
-    </Switch>
-  </Router>,
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/" component={ App } />
+        <Route path="/config" component={ ConfigDialog } />
+        <Route path="/auth" component={ AuthDialog } />
+      </Switch>
+    </Router>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
+
+/*
+border-left: thick solid #ff0000; 66 150 55
+*/
