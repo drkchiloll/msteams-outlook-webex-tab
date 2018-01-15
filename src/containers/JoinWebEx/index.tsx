564533552aed21325d46f4a7e9d8fe9ae8cbd792
drkchiloll <samwomack_@me.com>
2018-01-15 08:49:14 -0600

    Added JoinWebEx container

---
+++ src/containers/JoinWebEx/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import autobind from 'autobind-decorator';
+import {
+  FlatButton, Card, CardHeader
+} from 'material-ui';
+
+export class JoinWebEx extends React.Component<any,any> {
+  constructor(props) {
+    super(props);
+    microsoftTeams.initialize()
+  }
+
+  componentWillMount() {
+    microsoftTeams.getContext(({upn, subEntityId}) => {
+      if(subEntityId) {
+        // An "Instant" Meeting was Launched and the Team Member Clicked
+        // On the Join Conference Meeting
+        const entities: any = subEntityId;
+        if(entities.find(entity => entity.mail == upn)) {
+          window.open(entities.find(entity => entity.mail == upn).joinUrl, '_newTab');
+        }
+      }
+    });
+  }
+
+  render() {
+    return (
+      <FlatButton label='JOIN WEBEX'
+        style={{margineLeft: 500}}
+        labelPosition='after'
+        icon={<i className='mdi mdi-cisco-webex mdi-24px' />} />
+    )
+  }
+}
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,3 +1,4 @@
 export { App } from './App';
 export { AuthDialog } from './MSAuthDialog';
 export { ConfigDialog } from './MSConfigDialog';
+export { JoinWebEx } from './JoinWebEx';
