e5155c0ac5662de3793f81c6fdaa465b299a25fe
drkchiloll <samwomack_@me.com>
2018-02-03 10:31:28 -0600

    azure ad authentication

---
+++ src/middleware/azure.ts
@@ -0,0 +1,66 @@
+import { UserAgentApplication } from 'msx-silent';
+import { EventEmitter } from 'events';
+import * as Properties from '../../properties.json';
+const { msApp: { clientId, scopes } } = Properties;
+
+const client = new UserAgentApplication(
+  clientId, null,
+  (errDesc: string, token: string, err: string, tokenType: string) => {
+    if(tokenType === 'id_token') {
+      Msal.authEvent.emit('auth-cb-complete');
+    }
+  }, { cacheLocation: 'localStorage' }
+);
+
+
+export class Msal extends EventEmitter {
+  public static authEvent = new EventEmitter();
+
+  static callback(hash) {
+    return client.isCallback(hash)
+  }
+
+  static handleAuth(hash) {
+    client.handleAuthenticationResponse(hash);
+  }
+
+  static popup() {
+    return client.loginPopup(['openid']);
+  };
+
+  static tokenRedirect() {
+    const user = client.getUser();
+    if(user) {
+      client.acquireTokenRedirect(scopes);
+    } else {
+      this.redirect();
+    }
+  }
+
+  static redirect() {
+    client.loginRedirect(['openid']);
+  };
+
+  static silent() {
+    const user = client.getUser();
+    return client.acquireTokenSilent(scopes, null, user)
+      .catch((error: string) => ({ error }));
+  }
+ 
+  static getToken() {
+    const user = client.getUser();
+    return client.acquireTokenSilent(scopes, null, user);
+  };
+
+  static user() {
+    if(client.getUser()) {
+      return client.getUser();
+    } else {
+      return { name: '', identityProvider: '' };
+    }
+  }
+
+  static logout() {
+    client.logout();
+  }
+}
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,9 +1,9 @@
 import logger from './logger';
 export { logger };
+export { time } from './time-helper';
+export { Msal } from './azure';
 export {
   Api, ApiActions, WebExMeetingRequest,
   WebExAuth, WebExCreateMeetingParams,
   apiEmitter
 } from './Api';
-
-export { time } from './time-helper';
