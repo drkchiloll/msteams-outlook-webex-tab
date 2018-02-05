4f4f3054c2d794cb0ff88069a6a13c7957160148
drkchiloll <samwomack_@me.com>
2018-02-05 15:58:07 -0600

    adjustments for removal of MeetNow Button

--- src/components/MeetNow/index.tsx
+++ src/components/MeetNow/index.tsx
@@ -1,22 +1,16 @@
 import * as React from 'react';
 import * as Promise from 'bluebird';
-import * as moment from 'moment';
-import * as momenttz from 'moment-timezone';
+import { Participant, UserSearch } from '../index';
+import { Api } from '../../middleware';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import autobind from 'autobind-decorator';
 import {
-  Dialog, FlatButton, FontIcon, TextField,
-  CircularProgress, RaisedButton, Subheader,
-  List, Menu
+  Dialog, FlatButton, FontIcon,
+  CircularProgress, Subheader,
+  List, Menu, TextField
 } from 'material-ui';
 
-import { Participant } from '../Participant';
-import { UserSearch } from '../UserSearch';
-
-import { Api } from '../../middleware';
-
 const initialState = {
-  dialogOpen: false,
   members: null,
   agenda: '',
   launchBtn: 'LAUNCH',
@@ -28,13 +22,18 @@ export class WebExMeetNowDialog extends React.Component<any,any> {
   constructor(props) {
     super(props);
     this.state = {
-      dialogOpen: false,
       members: null,
       agenda: '',
       launchBtn: 'LAUNCH',
       attendees: [],
       organizer: {}
     };
+  }
+
+  componentWillMount() {
+    if(this.props.dialogOpen) {
+      this.getTeam();
+    }
   }
 
   @autobind
@@ -107,89 +106,78 @@ export class WebExMeetNowDialog extends React.Component<any,any> {
   }
 
   render() {
-    let api: Api = this.props.api;
-    let {webex} = this.props;
     const { attendees, organizer } = this.state;
-    const admin = <Participant user={JSON.parse(JSON.stringify(organizer))} />;
     return (
-      <div>
-        <RaisedButton
-          fullWidth={true}
-          style={{ width: 285}}
-          disabled={!webex.webExId || !webex.webExPassword}
-          primary={true}
-          labelColor='white'
-          label='MEET NOW'
-          labelPosition='after'
-          icon={<i className='mdi mdi-cisco-webex mdi-18px'
-            style={{
-              color: 'white',
-              fontSize: '1.1em' }} />}
-          onClick={this.getTeam} />
-        <Dialog title='Cisco WebEx Instant Meeting'
-          actions={[
-            <FlatButton label='Cancel' primary={true} onClick={() => {
-              this.resetState();
-            }} />,
-            <FlatButton
-              label={
-                this.state.launchBtn ||
-                <i className='mdi mdi-rotate-right mdi-spin mdi-24px' 
-                  style={{ verticalAlign: 'middle', color: '#673AB7' }} />
-              }
-              primary={true}
-              onClick={this.launchMeeting} />
-          ]}
-          modal={false}
-          open={this.state.dialogOpen}
-          style={{
-            position: 'relative',
-            height: 'auto', maxWidth: 'none', width: '100%'
-          }}
-          autoScrollBodyContent={true} >
-          <Grid>
-            <Row>
-              <Col sm={5}>
-                <TextField
-                  value={this.state.agenda}
-                  floatingLabelText='Meeting Agenda'
-                  floatingLabelFocusStyle={{fontSize: '1.5em'}}
-                  hintText='Optional Agenda Description'
-                  multiLine={true}
-                  fullWidth={true}
-                  autoFocus
-                  onChange={(e, value) => {
-                    this.setState({ agenda: value });
-                  }}
-                  rows={3} />
-              </Col>
-              <Col sm={7}>
-                <div style={{ marginLeft: '75px' }}>
-                  <Subheader> Organizer </Subheader>
-                  {admin}
-                  <Menu maxHeight={400} >
-                    <Subheader> Participants </Subheader>
-                    { attendees.length===0 ? <div></div> :
-                      JSON.parse(JSON.stringify(attendees)).map((att:any) =>
-                        <Participant user={att} key={att.id} remove={this.removeParticipant} />)}
-                  </Menu>
+      <div style={{ position: 'relative' }}>
+      <Dialog title='Cisco WebEx Instant Meeting'
+        actions={[
+          <FlatButton label='Cancel' primary={true} onClick={() => {
+            this.resetState();
+            this.props.close();
+          }} />,
+          <FlatButton
+            label={
+              this.state.launchBtn ||
+              <i className='mdi mdi-rotate-right mdi-spin mdi-24px' 
+                style={{ verticalAlign: 'middle', color: '#673AB7' }} />
+            }
+            primary={true}
+            onClick={this.launchMeeting} />
+        ]}
+        modal={false}
+        open={true}
+        style={{
+          maxWidth: 'none', width: '100%', height: 600
+        }}
+        autoScrollBodyContent={true} >
+        <Grid>
+          <Row>
+            <Col sm={5}>
+              <TextField
+                value={this.state.agenda}
+                floatingLabelText='Meeting Agenda'
+                floatingLabelFocusStyle={{fontSize: '1.5em'}}
+                hintText='Optional Agenda Description'
+                multiLine={true}
+                fullWidth={true}
+                autoFocus
+                onChange={(e, value) => {
+                  this.setState({ agenda: value });
+                }}
+                rows={3} />
+            </Col>
+            <Col sm={7}>
+              <div style={{ marginLeft: '75px' }}>
+                <div style={{ marginLeft: '20px', display: organizer.id ? 'none': 'inline' }}>
+                  <CircularProgress size={15} />
                 </div>
-              </Col>
-            </Row>
-            <Row>
-              <Col sm={12}>
-                <div style={{
-                  position: 'absolute',
-                  top: 215,
-                  width: '37%',
-                  marginTop: 0
-                }}>
-                  <UserSearch api={api} addAttendee={this.addParticipant} />
-                </div>
-              </Col>
-            </Row>
-          </Grid>
-        </Dialog>
+                <Subheader> Organizer </Subheader>
+                { organizer.id && organizer.displayName && organizer.mail ?
+                  <Participant user={JSON.parse(JSON.stringify(organizer))} /> :
+                  <div></div> }
+                <Menu maxHeight={400} >
+                  <Subheader> Participants </Subheader>
+                  { attendees.length===0 ? <div></div> :
+                    JSON.parse(JSON.stringify(attendees)).map((att:any) =>
+                      <Participant user={att} key={att.id} remove={this.removeParticipant} />)}
+                </Menu>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12}>
+              <div style={{
+                position: 'absolute',
+                top: 215,
+                width: '37%',
+                marginTop: 0
+              }}>
+                <UserSearch api={this.props.api} addAttendee={this.addParticipant} />
+              </div>
+            </Col>
+          </Row>
+        </Grid>
+      </Dialog>
       </div>
     )
   }
