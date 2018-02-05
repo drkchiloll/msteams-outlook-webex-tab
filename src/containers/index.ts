88d1f9167fd11ddec20600b76ee8046b4619f3a8
drkchiloll <samwomack_@me.com>
2018-02-05 09:47:58 -0600

    export the new container component..AdminConsent

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -2,3 +2,4 @@ export { App } from './App';
 export { AuthDialog } from './MSAuthDialog';
 export { ConfigDialog } from './MSConfigDialog';
 export { JoinWebEx } from './JoinWebEx';
+export { AdminConsent } from './AdminConsent';
